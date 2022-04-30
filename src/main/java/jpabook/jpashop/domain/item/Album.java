b833b90554ea195e03cab0ccb771ff2a7f77745b
임채성 <pan1129@naver.com>
2022-04-30 18:22:17 +0900

    엔티티 클래스 개발1 Commit

---
+++ src/main/java/jpabook/jpashop/domain/item/Album.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+
+@Entity
+@DiscriminatorValue("A")
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public class Album extends Item{
+
+    private String artist;
+    private String etc;
+
+}
