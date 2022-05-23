09a02cd824b959e03edb74cd996d0bb4de942b3f
임채성 <pan1129@naver.com>
2022-05-23 18:23:45 +0900

    주문 목록 검색 취소 Commit

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -28,37 +28,38 @@ public class OrderRepository {
     public List<Order> findAllByString(OrderSearch orderSearch){
 
         String jpql = "select o from Order o join o.member m";
-        boolean isFistCondition = true;
+        boolean isFirstCondition = true;
 
         //주문 상태 검색
-        if(orderSearch.getOrderStatus() != null){
-            if(isFistCondition){
-                jpql += "where";
-                isFistCondition = false;
-            }else{
-                jpql += "and";
+        if (orderSearch.getOrderStatus() != null) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
             }
-            jpql += "o.status = :status";
+            jpql += " o.status = :status";
         }
 
-        // 회원 이름 검색
-        if(StringUtils.hasText(orderSearch.getMemberName())){
-            if(isFistCondition){
-                jpql += "where";
-                isFistCondition = false;
-            }else{
-                jpql += "and";
+        //회원 이름 검색
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            if (isFirstCondition) {
+                jpql += " where";
+                isFirstCondition = false;
+            } else {
+                jpql += " and";
             }
-            jpql += "m.name like :name";
+            jpql += " m.name like :name";
         }
 
-        TypedQuery<Order> query = em.createQuery(jpql,Order.class)
-                .setMaxResults(1000); //최대 1000 건
-        if(orderSearch.getOrderStatus() != null){
-            query = query.setParameter("status",orderSearch.getOrderStatus());
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+                .setMaxResults(1000);
+
+        if (orderSearch.getOrderStatus() != null) {
+            query = query.setParameter("status", orderSearch.getOrderStatus());
         }
-        if(StringUtils.hasText(orderSearch.getMemberName())){
-            query = query.setParameter("name",orderSearch.getMemberName());
+        if (StringUtils.hasText(orderSearch.getMemberName())) {
+            query = query.setParameter("name", orderSearch.getMemberName());
         }
 
         return query.getResultList();
